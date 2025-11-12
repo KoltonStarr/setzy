d1dcc65aa8eb9f8c1c7e70c401ee9b4d4ecb7abb
KoltonStarr <koltonstarr@Koltons-MacBook-Pro.local>
2025-11-12 13:13:06 -0500

    cleaned up main.py imports

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import system_prompts
 from dotenv import load_dotenv
 from langchain_core.documents import Document
-from langchain_community.document_loaders import TextLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from pydantic import BaseModel, Field
 
