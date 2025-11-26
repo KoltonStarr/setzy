0aff2c78af68cca129756477da43188a30b1e4c5
KoltonStarr <koltonstarr@Koltons-MacBook-Pro.local>
2025-11-25 20:18:29 -0500

    CODE

---
+++ agent/README.md
@@ -0,0 +1,5 @@
+# Setzy Agent
+
+## Behavior
+The behavior of the agent is pretty simple:
+- It should be able to answer any and all questions about about the setter's call data.
--- agent/main.py
+++ agent/main.py
@@ -5,6 +5,7 @@ sys.path.insert(0, str(Path(__file__).parent.parent))
 # ===
 from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
+from system_prompt import SYSTEM_PROMPT
 from langchain.chat_models import init_chat_model
 from langchain.agents import create_agent
 from langchain.tools import tool
@@ -18,32 +19,13 @@ vector_store = Chroma(
     persist_directory=os.getenv("VECTOR_STORE_PERSIST_DIRECTORY"),
 )
 
-model = init_chat_model("gpt-4.1")
+agent = create_agent(init_chat_model("gpt-4.1"), tools=[], system_prompt=SYSTEM_PROMPT)
 
-@tool(response_format="content_and_artifact")
-def retrieve_context(query: str, k: int):
-    """Retrieve information to help answer a query."""
-    retrieved_docs = vector_store.similarity_search(query, k)
-    serialized = "\n\n".join(
-        (f"Source: {doc.metadata}\nContent: {doc.page_content}")
-        for doc in retrieved_docs
-    )
-    return serialized, retrieved_docs
-
-tools = [retrieve_context]
-# If desired, specify custom instructions
-prompt = (
-    "You have access to a tool that retrieves context from setter call data. "
-    "Use the tool to help answer user queries."
-)
-agent = create_agent(init_chat_model("gpt-4.1"), tools, system_prompt=prompt)
-
-query = (
-    "How many times does the word 'um' appear across all calls?"
-)
+print("What would you like to search for?")
+user_query = input("> ")
 
 for event in agent.stream(
-    {"messages": [{"role": "user", "content": query}]},
+    {"messages": [{"role": "user", "content": user_query}]},
     stream_mode="values",
 ):
     event["messages"][-1].pretty_print()
