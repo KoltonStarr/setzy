df2493e8eb4adaf657b94062127e432a84809e7f
KoltonStarr <koltonstarr@Koltons-MacBook-Pro.local>
2025-11-29 22:08:30 -0500

    Working version

--- agent/main.py
+++ agent/main.py
@@ -3,23 +3,15 @@ import os
 from pathlib import Path
 sys.path.insert(0, str(Path(__file__).parent.parent))
 # ===
-from langchain_chroma import Chroma
-from langchain_openai import OpenAIEmbeddings
 from system_prompt import SYSTEM_PROMPT
+from tools import vector_search
 from langchain.chat_models import init_chat_model
 from langchain.agents import create_agent
 from langchain.tools import tool
 from dotenv import load_dotenv
 load_dotenv()
 
-# Connect to existing vector store.
-vector_store = Chroma(
-    collection_name="sales_calls",
-    embedding_function=OpenAIEmbeddings(model="text-embedding-3-large"),
-    persist_directory=os.getenv("VECTOR_STORE_PERSIST_DIRECTORY"),
-)
-
-agent = create_agent(init_chat_model("gpt-4.1"), tools=[], system_prompt=SYSTEM_PROMPT)
+agent = create_agent(init_chat_model("gpt-5"), tools=[vector_search], system_prompt=SYSTEM_PROMPT)
 
 print("What would you like to search for?")
 user_query = input("> ")
--- agent/tools.py
+++ agent/tools.py
@@ -1,3 +1,31 @@
+import os
+from langchain.tools import tool
+from langchain_chroma import Chroma
+from langchain_core.documents import Document
+from langchain_openai import OpenAIEmbeddings
+from typing import Optional
+
 # A search tool for the vector database. 
+@tool 
+def vector_search(query: str, k_value: int, filters: Optional[dict]) -> list[Document]:
+    """Gets relevant data from a vector store to adequately answer a query.
+    
+    Args:
+        query (str): The query that will be used to get relevant data from the vector store.
+        k_value (int): Number of k-nearest neighbors
+        vector_store (Chroma): A full chroma instance.
+        filters: metadata filters for the query to the vector store.
+    
+    Returns:
+        list[Document]
+    """
+    vector_store = Chroma(
+        collection_name="sales_calls",
+        embedding_function=OpenAIEmbeddings(model="text-embedding-3-large"),
+        persist_directory=os.getenv("VECTOR_STORE_PERSIST_DIRECTORY"),
+    )
+    return vector_store.similarity_search(query, k=k_value, filter=filters)
+
+
 # A tool to read the embedding pipeline code if needed. 
 # A websearch tool.
