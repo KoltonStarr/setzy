9ec827b8b8a6e20341e52b3f1612aee6b6576a91
KoltonStarr <carnegieroyce@protonmail.com>
2026-01-06 19:46:13 -0500

    Added basic file upload functionality

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI, UploadFile
+from mypy_boto3_s3 import S3Client
 import boto3
 import os
 
@@ -12,3 +13,13 @@ async def upload_file(file: UploadFile):
     print(f"Filename: {file.filename}")
     print(f"content_type: {file.content_type}")
     print(f"file_size: {file.size}")
+
+    s3_client: S3Client = boto3.client('s3')
+    
+    s3_client.upload_fileobj(
+        Fileobj=file.file,
+        Bucket=s3_bucket_name,
+        Key=file.filename
+    )
+
+    return {"Status": 200}
